c4960e56251829ab7b5b101f139732573ae8a191
lety20391 <lety_20391@yahoo.com>
2018-05-22 00:37:39 +0700

    test read raw data from Arduino

--- Source Code/ePrj-Sem2/src/Dat_Le/Dat_Le.java
+++ Source Code/ePrj-Sem2/src/Dat_Le/Dat_Le.java
@@ -4,13 +4,36 @@
  * and open the template in the editor.
  */
 package Dat_Le;
-
+import com.fazecast.jSerialComm.*;
 /**
  *
  * @author Dat ThinkPad
  */
 public class Dat_Le {
+    
     public Dat_Le(){
         
     }
+    
+    public void TestArduino(){
+        SerialPort comPort = SerialPort.getCommPorts()[0];
+        comPort.openPort();
+        try {
+           while (true)
+           {
+              while (comPort.bytesAvailable() == 0)
+                 Thread.sleep(20);
+
+              byte[] readBuffer = new byte[comPort.bytesAvailable()];
+              int numRead = comPort.readBytes(readBuffer, readBuffer.length);
+              System.out.println("Read " + numRead + " bytes.");
+               for (byte b : readBuffer) {
+                   System.out.println((char)b);
+               }
+           }
+        } catch (Exception e) 
+        { System.out.println(e.getMessage()); }
+        comPort.closePort();
+    }
+    
 }
