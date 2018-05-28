cf29ef3a72079ae9c07f09d169ee12022e6cd39e
lety20391 <lety_20391@yahoo.com>
2018-05-28 19:29:34 +0700

    Revert "reanem"
    This reverts commit b0cac080808e35b4c908d7ecc82dbfded2d02caf.

--- Source Code/ePrj-Sem2/src/Duy/TaoTaiKhoan.java
+++ Source Code/ePrj-Sem2/src/Duy/TaoTaiKhoan.java
@@ -9,12 +9,12 @@ package Duy;
  *
  * @author DuDu
  */
-public class GuestSearch extends javax.swing.JFrame {
+public class TaoTaiKhoan extends javax.swing.JFrame {
 
     /**
      * Creates new form TaoTaiKhoan
      */
-    public GuestSearch() {
+    public TaoTaiKhoan() {
         initComponents();
     }
 
@@ -126,21 +126,20 @@ public class GuestSearch extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(GuestSearch.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TaoTaiKhoan.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(GuestSearch.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TaoTaiKhoan.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(GuestSearch.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TaoTaiKhoan.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(GuestSearch.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TaoTaiKhoan.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new GuestSearch().setVisible(true);
+                new TaoTaiKhoan().setVisible(true);
             }
         });
     }
